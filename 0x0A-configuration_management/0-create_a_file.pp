757f35116b9c3300b0916fca8784964629c84eeb
Favour James <Favourjames061@gmail.com>
2024-06-28 04:28:41 -0700

    Add new file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file in /tmp
 
 file { '/tmp/school/':
-ensure  => 'file'
-mode    => '0744'
-group   => 'www-data'
-owner   => 'www-data'
-content => 'I love Puppet'
+ensure  => 'file',
+mode    => '0744',
+group   => 'www-data',
+owner   => 'www-data',
+content => 'I love Puppet',
 }
