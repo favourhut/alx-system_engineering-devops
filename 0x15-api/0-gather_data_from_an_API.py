c7f560126fa37dcc879b066828d7079569234956
Favour <135041430+favourhut@users.noreply.github.com>
2024-08-02 05:27:13 +0100

    Get Todos

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,10 @@ from sys import argv
 if __name__ == "__main__":
     
     api_url = "https://jsonplaceholder.typicode.com"
-    request = requests.get(api_url + "user/{}".format(argv[1])).json
-    get_todo = requests.get(api_url + "todos/{}".format(argv[1])).json
+    req = requests.get(api_url + "user/{}".format(argv[1])).json
+    get_todo = requests.get(api_url + "todos", params={"userId" (argv[1])}).json
     
-    print(get_todo)
+    for i in get_todo:
+        result = i.get("title")
+        if i.get("completed") == True:
+            print("Employee {} is done with tasks({}/{})".format(req.get("name"), len("result"), len(get_todo)))
