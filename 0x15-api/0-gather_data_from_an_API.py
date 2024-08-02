154bfc596ce6d47f8a1f8bfed5b744faee5bad87
Favour <135041430+favourhut@users.noreply.github.com>
2024-08-02 05:39:43 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,10 +12,9 @@ if __name__ == "__main__":
     req = requests.get(api_url + "user/{}".format(argv[1])).json
     get_todo = requests.get(api_url + "todos", params={"userId" (argv[1])}).json
     
-    for i in get_todo:
-        result = i.get("title")
-        if i.get("completed") == True:
-            print("Employee {} is done with tasks({}/{})".format(req.get("name"), len("result"), len(get_todo)))
+    result = [i.get("title") for i in get_todo if i.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        req.get("name"), len("result"), len(get_todo)))
             
-            for i in result:
-                print("\t {}".format(i))
+    for i in result:
+        print("\t {}".format(i))
