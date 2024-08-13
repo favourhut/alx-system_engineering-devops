2bf1b5e78cfdd76deb4fc6f9d44dc4b3d4562da1
Favour <135041430+favourhut@users.noreply.github.com>
2024-08-13 20:51:38 +0100

    Checker failed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Function to query the Reddit API"""
+"""Gets subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    
-    response = requests.get(api_url, headers=headers, allow_redirects=False)
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    else:
-        results = response.json().get("data")
-        return results.get("subscribers")
+    results = response.json().get("data")
+    return results.get("subscribers")
