85c6872d52e72da6beae0ca136260a7fe01677f7
Favour <135041430+favourhut@users.noreply.github.com>
2024-08-13 20:17:57 +0100

    Task 1 completed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""Function to query the Reddit API
-and return the number of
+"""Function to query the Reddit API and return the number of 
 subscribers for a given subreddit"""
 
 
-import json
 import requests
-
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit.
@@ -17,23 +14,14 @@ def number_of_subscribers(subreddit):
     Returns:
         int: The number of subscribers, or 0 if the subreddit is invalid.
     """
-
-    api_url = "https://www.reddit.com/r/{}/about.json"
-    HEADER = {"User-Agent": "custome-agent"}
     
-    try:
-        url = api_url.format(subreddit)
-        
-        response = requests.get(url, headers=HEADER, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = json.loads(response.text)
-            subscribers = data["data"]["subscribers"]
-            return subscribers
-             
-        else:
-            return 0
-       
-    except Exception:
-        """If no valid subreddit return 0"""
+    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "custom-agent"}
+    
+    response = requests.get(api_url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        results = response.json().get("data")
+        total_sub = results.get("subscribers")
+        return total_sub
+    else:
         return 0
