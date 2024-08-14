d25d6ded2bcc617a67b03023c14118e7f15be1a5
Favour James <Favourjames061@gmail.com>
2024-08-14 19:25:10 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fied nad file extention
 
-exec{'fix-wordpress':
-    command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-    path     => '/bin/';
+exec{ 'fix-wordpress':
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/bin/';
 }
